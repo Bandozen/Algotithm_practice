8162e66c68b2b3b1a4b219316c5835f0ebbc7a2c
조찬익 <whcksdlr5909@gmail.com>
2023-03-09 17:46:28 +0900

    "타겟넘버 풀이!"

---
+++ 프로그래머스/타겟넘버.py
@@ -0,0 +1,20 @@
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    q = deque()
+    q.append([numbers[0], 0])
+    q.append([-1*numbers[0], 0])
+    while q:
+        tmp, idx = q.popleft()
+        idx += 1
+        if idx < len(numbers):
+            q.append([tmp+numbers[idx], idx])
+            q.append([tmp-numbers[idx], idx])
+        else:
+            if tmp == target:
+                answer += 1
+    return answer
+
+numbers = [1, 1, 1, 1, 1]
+target = 3
+print(solution(numbers, target))
