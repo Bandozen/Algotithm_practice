489a34c4e367ac8bb082ae7753d11fcd38285171
조찬익 <whcksdlr5909@gmail.com>
2023-03-14 21:27:52 +0900

    "백준 풀이!"

---
+++ 백준/백준1697번.py
@@ -0,0 +1,23 @@
+from collections import deque
+import sys
+
+
+def bfs():
+    q = deque()
+    q.append(n)
+    while q:
+        x = q.popleft()
+        if x == k:
+            print(d[x])
+            break
+        for nx in (x-1, x+1, x*2):
+            if 0 <= nx <= maxV and not d[nx]:
+                d[nx] = d[x] + 1
+                q.append(nx)
+
+
+maxV = 10 ** 5
+d = [0] * (maxV + 1)
+n, k = map(int, sys.stdin.readline().split())
+
+bfs()
